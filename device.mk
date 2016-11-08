9b1c1542af31dd0995b2ef1559bbcc60e39fff85
rubencarneiro <rubencarneiro01@gmail.com>
2016-11-08 21:46:35 +0000

    Update Ubuntu specific Files

--- device.mk
+++ device.mk
@@ -102,9 +102,6 @@ PRODUCT_COPY_FILES += \
     device/lge/hammerhead/ubuntu/etc/wifi/bcmdhd.cal:system/ubuntu/etc/wifi/bcmdhd.cal \
     device/lge/hammerhead/ubuntu/lib/udev/rules.d/70-android.rules:system/ubuntu/lib/udev/rules.d/70-android.rules \
     device/lge/hammerhead/ubuntu/usr/bin/hciattach:system/ubuntu/usr/bin/hciattach \
-    device/lge/hammerhead/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/HiFi:system/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/HiFi \
-    device/lge/hammerhead/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/msm8974-taiko-mtp-snd-card.conf:system/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/msm8974-taiko-mtp-snd-card.conf \
-    device/lge/hammerhead/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/VoiceCall:system/ubuntu/usr/share/alsa/ucm/msm8974-taiko-mtp-snd-card/VoiceCall \
     device/lge/hammerhead/ubuntu/usr/share/bluetooth-touch/hammerhead:system/ubuntu/usr/share/bluetooth-touch/hammerhead \
     device/lge/hammerhead/ubuntu/usr/share/powerd/device_configs/config-default.xml:system/ubuntu/usr/share/powerd/device_configs/config-default.xml
 
