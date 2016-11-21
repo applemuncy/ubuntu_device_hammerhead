ba3cbfbc5928714d4cbeab894510e622f43e2f0c
rubencarneiro <rubencarneiro01@gmail.com>
2016-11-21 22:37:46 +0000

    Add missing adb configuration Hammerhead

--- device.mk
+++ device.mk
@@ -386,8 +386,9 @@ $(call inherit-product, device/lge/hammerhead/mdt.mk)
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/ubuntu/udev.rules:system/ubuntu/lib/udev/rules.d/70-android.rules \
     device/lge/hammerhead/ubuntu/powerd-config.xml:system/ubuntu/usr/share/powerd/device_configs/config-default.xml \
+		device/lge/hammerhead/ubuntu/android-tools-adb.conf:system/ubuntu/etc/init/android-tools-adb.conf \
     device/lge/hammerhead/ubuntu/display.conf:system/ubuntu/etc/ubuntu-touch-session.d/android.conf
-		
+
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
